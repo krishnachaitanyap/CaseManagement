f3e315b33a1fcc8196de85aa3d56b4f62d41d2fe
Krishna Chaitanya <palivela.chaitu@gmail.com>
2017-01-16 13:48:28 +0530

    Initiating Casemenement Client

---
+++ CaseManagemnt-cli/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CaseManagemntCliPage } from './app.po';
+
+describe('case-managemnt-cli App', function() {
+  let page: CaseManagemntCliPage;
+
+  beforeEach(() => {
+    page = new CaseManagemntCliPage();
+  });
+
+  // it('should display message saying app works', () => {
+  //   page.navigateTo();
+  //   expect(page.getParagraphText()).toEqual('app works!');
+  // });
+});
